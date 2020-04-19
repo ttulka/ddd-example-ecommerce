3b2fc16196742bc5b1fb7a58b009333fa31f7011
Tomas Tulka <tomas.tulka@gmail.com>
2020-04-19 14:22:22 +0200

    async support enabled

--- src/main/java/com/ttulka/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ttulka/ecommerce/ECommerceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
@@ -13,6 +14,7 @@ import nz.net.ultraq.thymeleaf.LayoutDialect;
  * Spring Boot based monolithic application.
  */
 @SpringBootApplication
+@EnableAsync
 public class ECommerceApplication {
 
     public static void main(String[] args) {
